b12fb40ec54dc39938b02d5bec8bb0d475305315
H-raf <ibnmrabet.achraf@gmail.com>
2017-01-12 19:44:25 +0100

    Packages

---
+++ src/main/java/metronome/Test/MoteurTest.java
@@ -0,0 +1,98 @@
+package src.main.java.metronome.Test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by H-raf on 12/01/2017.
+ */
+public class MoteurTest {
+    @Before
+    public void setUp() throws Exception {
+
+        
+    }
+
+    @Test
+    public void init() throws Exception {
+
+    }
+
+    @Test
+    public void getTempo() throws Exception {
+
+    }
+
+    @Test
+    public void getMesure() throws Exception {
+
+    }
+
+    @Test
+    public void setEnMarche() throws Exception {
+
+    }
+
+    @Test
+    public void getEnMarche() throws Exception {
+
+    }
+
+    @Test
+    public void setCmdMarquerTemps() throws Exception {
+
+    }
+
+    @Test
+    public void setCmdMarquerMesure() throws Exception {
+
+    }
+
+    @Test
+    public void play() throws Exception {
+
+    }
+
+    @Test
+    public void incTempo() throws Exception {
+
+    }
+
+    @Test
+    public void decTempo() throws Exception {
+
+    }
+
+    @Test
+    public void incMesure() throws Exception {
+
+    }
+
+    @Test
+    public void decMesure() throws Exception {
+
+    }
+
+    @Test
+    public void setMesure() throws Exception {
+
+    }
+
+    @Test
+    public void setTempo() throws Exception {
+
+    }
+
+    @Test
+    public void setCmdUpdateMesure() throws Exception {
+
+    }
+
+    @Test
+    public void setCmdUpdateTempo() throws Exception {
+
+    }
+
+}
